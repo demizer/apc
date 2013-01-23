cc25973d081b4d7f1b3c940d8b7877550ae5b74f
Jesus Alvarez <jeezusjr@gmail.com>
2013-01-23 00:39:45 -0800

    Add support for adding multiple packages to a repo
    Instead of looping over a list and adding one package at a time, this update
    adds all of the packages at once, per arch.

--- pbldr/repo.py
+++ pbldr/repo.py
@@ -53,6 +53,43 @@ def add_package(target, package_obj, keyid):
     return True
 
 
+def _prepare_repo_target(target, package_list, arch):
+    """Prepares the repo target.
+
+    This function first removes the old packages and then copies in the new
+    packages.
+
+    :target: The repo target
+    :package_list: A list of package objects
+    :arch: The architecture of the repo to prepare
+    :returns: A list of packages to be added to a repository
+
+    """
+    rlist = []
+    for pkg in package_list:
+        if not pkg['arch'] == arch:
+            continue
+        gpat = os.path.join(target, arch, pkg['name'] + '*')
+        if util.run('rm -rf ' + gpat, True) > 0:
+            logr.warning('Could not remove existing packages at ' + gpat)
+            logr.warning('      or no packages not found')
+
+        ppath = os.path.join(os.getcwd(), 'stage', pkg['name'])
+        log('Copying {} to {}'.format(pkg['name'], target))
+        rcmd = 'cp {0}*/*{1}.pkg.tar* {2}/{1}/'.format(ppath, arch, target)
+        if util.run(rcmd, True) > 1:
+            logr.error('Could not move the package to the repo')
+            return False
+
+        ctmp = 'cp {0}*/*.src.tar* {1}/sources/'
+        if util.run(ctmp.format(ppath, target), True) > 1:
+            logr.warning('Could not move the package source to the repo')
+
+        rlist.append(pkg['filename'])
+
+    return rlist
+
+
 def add_package_list(target, package_list, keyid):
     '''Adds a list of packages to repository.
 
@@ -63,39 +100,20 @@ def add_package_list(target, package_list, keyid):
 
     '''
     log('\nAdding packages to ' + target)
-    rlist = []
-    for pkg in package_list:
-        gpat = os.path.join(target, pkg['arch'], pkg['name'] + '*')
-        if util.run('rm -rf ' + gpat, True) > 0:
-            logr.warning('Could not remove existing packages at ' + gpat)
-            logr.warning('      or no packages not found')
 
-        ppath = os.path.join(os.getcwd(), 'stage', pkg['name'])
-        log('Copying {} to {}'.format(pkg['name'], target))
-        rcmd = 'cp {0}*/*{1}.pkg.tar* {2}/{1}/'.format(ppath, pkg['arch'],
-                                                       target)
-        if util.run(rcmd, True) > 1:
-            logr.error('Could not move the package to the repo')
-            return False
+    mkcmd = 'mkdir -p {}/{{i686,x86_64,sources}}'.format(target)
+    if util.run(mkcmd, True, util.subprocess.DEVNULL) > 0:
+        logr.warning('Could not create the repo subdirectories')
 
-        # Copy the package source in stage to the repo directory
-        ctmp = 'cp {0}*/*.src.tar* {1}/sources/'
-        if util.run(ctmp.format(ppath, target), True) > 1:
-            logr.warning('Could not move the package source to the repo')
-
-        rlist.append(pkg['filename'])
-
-    if rlist:
+    for arch in ('x86_64', 'i686'):
+        log('Adding packages to the ' + arch + ' repository.')
+        rlist = _prepare_repo_target(target, package_list, arch)
         rname = os.path.basename(os.getcwd())
-        log('Adding {} to the {} {} repository.'.format(pkg['filename'], rname,
-                                                        pkg['arch']))
-        rpath = os.path.join(target, pkg['arch'])
+        rpath = os.path.join(target, arch)
         rcmd = ['repo-add', '-s', '-k', keyid, rname + '.db.tar.xz']
         rcmd.extend(rlist)
         if util.run_in_path(rpath, rcmd, False) > 0:
             logr.error('Could not add the packages to the repo')
             return False
 
-        return True
-
-    return False
+    return True
