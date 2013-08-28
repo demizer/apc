1427ae3b8b58eec6a23367f80929f8494617818e
Jesus Alvarez <jeezusjr@gmail.com>
2013-08-28 01:14:33 -0700

    main.go: A list of external packages is printed

--- src/apc/main.go
+++ src/apc/main.go
@@ -5,23 +5,146 @@
  *
  * MIT Licensed. See LICENSE for details.
  */
+
 package main
 
 import (
+	"alpm"
 	"logger"
 	"os"
-	"time"
 )
 
-type Package struct {
-	Name         string
-	LastRetrieve time.Time
-	Installed    bool
-	Info         *os.FileInfo
-	Repo         string
-	Path         string
+// ParseConfig parses the pacman config if it exists and returns a PacmanConfig
+// object.
+func ParseConfig() (*alpm.PacmanConfig, error) {
+	fconf, err := os.Open(PACMAN_CONF_PATH)
+	if err != nil {
+		return nil, err
+	}
+	conf, err := alpm.ParseConfig(fconf)
+	if err != nil {
+		return nil, err
+	}
+	return &conf, nil
+}
+
+// An officially supported repository that would be found in pacman.conf.
+type OfficialDb int
+
+const (
+	UnofficialDb OfficialDb = 1 + iota
+	Core
+	Extra
+	Community
+	Testing
+	Multilib
+	MultilibTesting
+	Local
+)
+
+var dbs = [...]string{
+	"UnofficialDb",
+	"Core",
+	"Extra",
+	"Community",
+	"Testing",
+	"Multilib",
+	"MultilibTesting",
+	"Local",
+}
+
+// Returns the english name of the official package database.
+func (db OfficialDb) String() string { return dbs[db-1] }
+
+// Returns the integer equivalent of name as a OfficialDb type. Nil is
+// returned if name is not an officially supported repository.
+func OfficialDbFromString(name string) OfficialDb {
+	if name == "core" {
+		return Core
+	} else if name == "extra" {
+		return Extra
+	} else if name == "community" {
+		return Community
+	} else if name == "testing" {
+		return Testing
+	} else if name == "multilib" {
+		return Multilib
+	} else if name == "multilib-testing" {
+		return MultilibTesting
+	}
+	return UnofficialDb
+}
+
+var (
+	pacmanConf  *alpm.PacmanConfig
+	handle      *alpm.Handle
+	officialDbs = make(map[OfficialDb]alpm.Db)
+	localDb     *alpm.Db
+)
+
+// Determines if the package is from an officially supported repo such as core,
+// extra, or community.
+func IsOfficialPackage(pkg alpm.Package) bool {
+	name := pkg.Name()
+	for _, db := range officialDbs {
+		if _, err := db.PkgByName(name); err == nil {
+			// TODO: Fix logger to allow setting log level on
+			// default logger!!
+			// log.Debugln("IsOfficialPackage():", name, "is from",
+				// db.Name())
+			return true
+		}
+	}
+	// log.Debugln("IsOfficialPackage():", name, "is NOT an official package")
+	return false
+}
+
+func InitPacmanDbs() error {
+	dbs, err := handle.SyncDbs()
+	if err != nil {
+		return err
+	}
+	for _, db := range dbs.Slice() {
+		if odb := OfficialDbFromString(db.Name()); odb > UnofficialDb {
+			log.Debugln("Detected official db:", odb)
+			officialDbs[odb] = db
+		}
+	}
+	if localDb, err = handle.LocalDb(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func Init() {
+	pacmanConf, err := ParseConfig()
+	if err != nil {
+		log.Criticalln(err)
+		os.Exit(1)
+	}
+	handle, err = pacmanConf.CreateHandle()
+	if err != nil {
+		log.Criticalln(err)
+		os.Exit(1)
+	}
+	if err := InitPacmanDbs(); err != nil {
+		log.Criticalln(err)
+		os.Exit(1)
+	}
 }
 
 func main() {
-	log.Debugln("Hello, world!")
+	// Read the config and get the handle
+	Init()
+
+	for _, pkg := range localDb.PkgCache().Slice() {
+		if !IsOfficialPackage(pkg) {
+			log.Println(pkg.Name())
+		}
+	}
+
+	if handle.Release() != nil {
+		log.Criticalln("Could not release libalpm!")
+		os.Exit(1)
+	}
 }
